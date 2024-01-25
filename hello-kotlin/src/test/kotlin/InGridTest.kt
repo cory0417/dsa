35aa785e8cffbef44d511f1147ab998326d90103
Daeyoung Kim <corydykim@gmail.com>
2024-01-25 08:26:18 -0500

    Add jUnit tests

--- hello-kotlin/src/test/kotlin/InGridTest.kt
+++ hello-kotlin/src/test/kotlin/InGridTest.kt
@@ -1,10 +1,26 @@
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
 
-class MainKtTest {
+class GridTest {
 
     @Test
-    fun inGrid() {
+    fun testInGridWithinBounds() {
+        assertTrue(inGrid(50, 100))
+        assertTrue(inGrid(0, 0))
+        assertTrue(inGrid(255, 255))
+    }
+
+    @Test
+    fun testInGridOutsideBounds() {
+        assertFalse(inGrid(-1, 100))
+        assertFalse(inGrid(256, 200))
+        assertFalse(inGrid(50, -5))
+        assertFalse(inGrid(100, 300))
+    }
+
+    @Test
+    fun testInGridEdgeCases() {
+        assertFalse(inGrid(-1, -1))
+        assertFalse(inGrid(256, 256))
     }
 }
