68aa8daf3fc1d2eed9c22d57fb701493b9bf7ee4
Daeyoung Kim <corydykim@gmail.com>
2024-02-11 09:08:15 -0500

    Add additional tests for the MyGraph class

--- assignments/03-graphs-traversal/src/test/kotlin/GraphTest.kt
+++ assignments/03-graphs-traversal/src/test/kotlin/GraphTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertEquals
 
 class MyGraphTest {
     @Test
-    fun testAddVertices() {
+    fun testAddGetVertices() {
         val g = MyGraph<String>()
         g.addEdge("a", "b", 1.0)
         g.addEdge("a", "c", 2.0)
@@ -32,6 +32,16 @@ class MyGraphTest {
 
         assertEquals(g.getEdges("a").size, 2)
         assertEquals(g.getEdges("a"), mapOf("b" to 1.0, "c" to 2.0))
+    }
+
+    @Test
+    fun testClear() {
+        val g = MyGraph<String>()
+        g.addEdge("a", "b", 1.0)
+        g.clear()
+
+        assertEquals(g.getVertices(), setOf<String>())
+        assertEquals(g.getEdges("a"), mapOf<String, Double>())
     }
 
 }
