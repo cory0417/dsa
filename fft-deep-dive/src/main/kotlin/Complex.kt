4c60b1f62459f693b03ebde240dd3d696cdbb2e7
Daeyoung Kim <corydykim@gmail.com>
2024-03-14 00:27:38 -0400

    Add convenience methods for Complex

--- fft-deep-dive/src/main/kotlin/Complex.kt
+++ fft-deep-dive/src/main/kotlin/Complex.kt
@@ -1,6 +1,20 @@
+import kotlin.math.atan2
+import kotlin.math.hypot
+
 data class Complex(val real: Double, val imaginary: Double) {
-    operator fun plus(other: Complex) = Complex(real + other.real, imaginary + other.imaginary)
-    operator fun minus(other: Complex) = Complex(real - other.real, imaginary - other.imaginary)
+    operator fun plus(other: Complex) =
+        Complex(real + other.real, imaginary + other.imaginary)
+
+    operator fun minus(other: Complex) =
+        Complex(real - other.real, imaginary - other.imaginary)
+
     operator fun times(other: Complex) =
-        Complex(real * other.real - imaginary * other.imaginary, real * other.imaginary + imaginary * other.real)
+        Complex(
+            real * other.real - imaginary * other.imaginary,
+            real * other.imaginary + imaginary * other.real
+        )
+
+    fun abs() = hypot(real, imaginary)
+
+    fun phase() = atan2(imaginary, real)
 }
