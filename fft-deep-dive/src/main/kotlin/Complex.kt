c4283fb6757ef21378a4698bf28f8d2a87b78e93
Daeyoung Kim <corydykim@gmail.com>
2024-03-14 08:24:39 -0400

    Add docstrings

--- fft-deep-dive/src/main/kotlin/Complex.kt
+++ fft-deep-dive/src/main/kotlin/Complex.kt
@@ -3,6 +3,14 @@ package org.example
 import kotlin.math.atan2
 import kotlin.math.hypot
 
+/**
+ * A class to represent complex numbers.
+ *
+ * @property real The real part of the complex number.
+ * @property imaginary The imaginary part of the complex number.
+ * @constructor Creates a new complex number with the given [real] and
+ * [imaginary] parts.
+ */
 data class Complex(val real: Double, val imaginary: Double) {
     operator fun plus(other: Complex) =
         Complex(real + other.real, imaginary + other.imaginary)
